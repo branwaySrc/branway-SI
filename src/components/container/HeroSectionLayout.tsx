17c5c50f16163f5da428a3f793595b3f7ebce42b
branway <contact@branway.kr>
2024-03-08 15:29:33 +0900

    glassmorphism

--- src/components/container/HeroSectionLayout.tsx
+++ src/components/container/HeroSectionLayout.tsx
@@ -4,6 +4,7 @@ import {
   Section,
   AbsoluteBg,
 } from "@/manager/lib/HTMLElements";
+import Image from "next/image";
 
 interface HeroSectionTm {
   badgeLabel?: string;
@@ -15,27 +16,28 @@ interface HeroSectionTm {
 export const HeroSectionLayout = (props: HeroSectionTm) => {
   return (
     <Section className="border-[1px] border-[#FFFFFF18] bg-[#000000] relative">
-      <Section className="border-[1px] border-[#FFFFFF18] m-6 bg-[#ffffff02] relative">
-        <Section className="z-10 flex flex-col items-center border-[#FFFFFF18] h-[430px] border-[1px] m-8 bg-black">
-          <Container className="mt-12">
+      <Image src={"/darkwall.jpg"} fill alt="background" className="absolute" />
+      <Section className="border-[1px] border-[#FFFFFF18] m-5 bg-[#00000050] relative bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-80">
+        <Section className="z-10 flex flex-col items-center border-[#FFFFFF18] h-[430px] border-[1px] m-7 bg-black bg-opacity-50">
+          <Container className="mt-8">
             {props.badgeLabel && (
               <Box className="text-sm font-bold bg-gradient-to-r from-indigo-400 to-cyan-400 inline-block text-transparent bg-clip-text">
                 {props.badgeLabel}
               </Box>
             )}
           </Container>
-          <Container className="flex justify-center mt-[76px]">
+          <Container className="flex justify-center mt-[60px]">
             <Box className="font-black text-3xl max-w-[80%] text-center leading-[40px]">
               {props.title || "Title"}
             </Box>
           </Container>
-          <Container className="flex justify-center mt-20 relative">
+          <Container className="flex justify-center mt-16 relative">
             <Box className="text-slate-400 max-w-[80%] text-center leading-7 text-sm">
               {props.desc || "desc"}
             </Box>
           </Container>
         </Section>
-        <Container className="absolute border-t-[1px] border-[#FFFFFF18] w-full h-[70%] bottom-0" />
+        <Container className="absolute border-t-[1px] border-[#FFFFFF18] w-full h-[75%] bottom-0" />
         <Container className="absolute border-t-[1px] border-[#FFFFFF18] w-full h-[40%] bottom-0" />
       </Section>
     </Section>
